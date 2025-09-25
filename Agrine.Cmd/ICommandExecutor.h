6590ebba919f2c0354c21df837b9ecb7fd90ed7f
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-26 00:57:12 +0330

    - Update ICommandExecutor.h Base Code

--- Agrine.Cmd/ICommandExecutor.h
+++ Agrine.Cmd/ICommandExecutor.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <string>
+
+/**
+ * @brief Interface for command executors
+ */
+class ICommandExecutor {
+public:
+    virtual ~ICommandExecutor() = default;
+
+    /**
+     * @brief Executes a given command and returns the result.
+     * @param command Command string to execute
+     * @return Output from the command execution
+     */
+    virtual std::string Execute(const std::string& command) = 0;
+};
