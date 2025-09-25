fc9b60a649eb0d45d356236ea1038fc1d37e4732
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-25 18:42:41 +0330

    - Update AgrineCMDAPI.h Base Code

--- Agrine.Cmd/AgrineCmdAPI.h
+++ Agrine.Cmd/AgrineCmdAPI.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+
+#ifdef AGRINECMD_EXPORTS
+#define AGRINECMD_API __declspec(dllexport)
+#else
+#define AGRINECMD_API __declspec(dllimport)
+#endif
+
+extern "C" {
+    AGRINECMD_API const char* RunCmd(const char* command);
+    AGRINECMD_API const char* RunPowerShell(const char* command);
+}
