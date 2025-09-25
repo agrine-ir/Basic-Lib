c619e0ff97927ea4bdaaa677795884435c09013e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-26 01:01:30 +0330

    - Update AgrineCmdAPI.cpp Base Code

--- Agrine.Cmd/AgrineCmdAPI.cpp
+++ Agrine.Cmd/AgrineCmdAPI.cpp
@@ -3,15 +3,31 @@
 #include "CmdExecutor.h"
 #include "PowerShellExecutor.h"
 
-// Static buffer to hold output
-static std::string resultBuffer;
+#include <mutex>
+#include <unordered_map>
+
+/**
+ * @brief Thread-safe storage for result strings
+ */
+static std::mutex resultMutex;
+static std::unordered_map<std::thread::id, std::string> resultStorage;
 
 const char* RunCmd(const char* command) {
-    resultBuffer = CmdExecutor::RunCommand(command);
-    return resultBuffer.c_str();
+    CmdExecutor executor;
+    std::string result = executor.Execute(command);
+
+    std::lock_guard<std::mutex> lock(resultMutex);
+    resultStorage[std::this_thread::get_id()] = result;
+
+    return resultStorage[std::this_thread::get_id()].c_str();
 }
 
 const char* RunPowerShell(const char* command) {
-    resultBuffer = PowerShellExecutor::RunCommand(command);
-    return resultBuffer.c_str();
+    PowerShellExecutor executor;
+    std::string result = executor.Execute(command);
+
+    std::lock_guard<std::mutex> lock(resultMutex);
+    resultStorage[std::this_thread::get_id()] = result;
+
+    return resultStorage[std::this_thread::get_id()].c_str();
 }
