fcf9eb1e7ef0935175aed2a75af2faadbc8d9c99
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-28 14:09:18 +0330

    - Update Agrine.Cmd.h Base Code

--- Agrine.Cmd/Agrine.Cmd.h
+++ Agrine.Cmd/Agrine.Cmd.h
@@ -1,11 +1,25 @@
 #pragma once
 
+// Main header file for the Agrine.Cmd library
+// This header exposes the public API of the DLL.
+
 using namespace System;
 
-namespace AgrineCmd {
-	public ref class Class1
-	{
-		// TODO: Add your methods for this class here.
-	};
-	
+namespace Agrine {
+	namespace Cmd {
+
+		public ref class CmdExecutor
+		{
+		public:
+			// Executes a command using CMD.exe and returns the output.
+			static String^ Execute(String^ command);
+		};
+
+		public ref class PowerShellExecutor
+		{
+		public:
+			// Executes a command using PowerShell and returns the output.
+			static String^ Execute(String^ command);
+		};
+	}
 }
