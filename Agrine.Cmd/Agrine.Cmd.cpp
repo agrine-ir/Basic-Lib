bd085cbda47653f750c30bf06d0e7eacb3491df7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-28 14:18:12 +0330

    Update Agrine.Cmd.cpp Base Code

--- Agrine.Cmd/Agrine.Cmd.cpp
+++ Agrine.Cmd/Agrine.Cmd.cpp
@@ -1,4 +1,6 @@
 #include "pch.h"
-
 #include "Agrine.Cmd.h"
 
+// This file serves as the entry point for the DLL.
+// Currently, it just ensures the library compiles correctly.
+// No direct code is needed here unless you want DllMain for native hooks.
