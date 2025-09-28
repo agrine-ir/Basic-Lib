798639d5f2b0ac1a01de44e71d40d6640e20f89b
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-28 17:08:42 +0330

    - Add Core Folder To Header Folder In Agrine.Math

---
+++ TestPro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Agrine.Cmd;
+
+namespace TestPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Microsoft");
+            string result = Agrine.Cmd.PowerShellExecutor.Execute("Get-Location");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
