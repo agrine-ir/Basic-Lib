96395e2a7980af0862a2a9453fc191186178f7fd
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-28 14:11:21 +0330

    - Update CmdExecutor.h Base Code

--- Agrine.Cmd/CmdExecutor.h
+++ Agrine.Cmd/CmdExecutor.h
@@ -1,1 +1,15 @@
 #pragma once
+
+using namespace System;
+
+namespace Agrine {
+    namespace Cmd {
+
+        public ref class CmdExecutor
+        {
+        public:
+            static String^ Execute(String^ command);
+        };
+
+    }
+}
