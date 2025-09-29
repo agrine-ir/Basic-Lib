01c2ec5a1f53f92f4d9bc986f167d2999e59237c
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-29 23:58:25 +0330

    - Update Agrine.Math.h Base Code (Add Based Project Includes)

--- Agrine.Math/Agrine.Math.h
+++ Agrine.Math/Agrine.Math.h
@@ -1,5 +1,41 @@
 #pragma once
 
-using namespace System;
+// Master include for Agrine.Math library
+// Consumers of the library should only need to include this file.
 
 #include "pch.h"
+
+// Core
+#include "Types.h"
+#include "Constants.h"
+#include "Exceptions.h"
+#include "Utils.h"
+
+// Algebra
+#include "Vector.h"
+#include "Matrix.h"
+#include "Polynomial.h"
+
+// Calculus
+#include "Differentiation.h"
+#include "Integration.h"
+#include "Series.h"
+#include "DiffEq.h"
+
+// Geometry
+#include "Analytic.h"
+#include "Euclidean.h"
+#include "Computational.h"
+#include "Topology.h"
+
+// Probability
+#include "Distributions.h"
+#include "Statistics.h"
+#include "Randgen.h"
+
+// Advanced Math
+#include "Complex.h"
+#include "Fourier.h"
+#include "Laplace.h"
+#include "Numerics.h"
+#include "Optimization.h"
